2ed3933a1ce96be1491c0b1a658b996fae2bdf6a
Timo Franz <timo@dumpark.com>
2015-05-05 20:52:03 +1200

    rework routing ... work in progress

--- app/src/app.js
+++ app/src/app.js
@@ -43,29 +43,25 @@ var log             = require('debug')('src:app')
 //TODO set with config | environment variable
 localStorage.setItem("debug", "*");
 
-log('routes', routes)
-
-
 var router = Router.create({
   routes: routes,
   location: Router.HashLocation
 })
 
-log('router', router)
 
-var key = '1nmW8b_2HDgMzvuyllWCSV2hc8uUpyNrTT0WAC_7MnhE'
-var pathname = (window.history && window.history.state) ? window.history.state.path : '/'
+//var key = '1nmW8b_2HDgMzvuyllWCSV2hc8uUpyNrTT0WAC_7MnhE'
+//var pathname = (window.history && window.history.state) ? window.history.state.path : '/'
 
 var stores = {
-  routes: new RouteStore({ router: router, pathname: pathname  }),
-  data: new DataStore({ key: key, sheet: 'data', loadData: loadData  }),
-  dataBreakdowns: new DataBreakdownStore({ key: key, sheet: 'data_breakdowns', loadData: loadData  }),
-  dataGroups: new DataGroupStore({ key: key, sheet: 'data_groups', loadData: loadData  }),
-  dataSources: new DataSourceStore({ key: key, sheet: 'data_sources', loadData: loadData }),
-  dataTypes: new DataTypeStore({ key: key, sheet: 'data_types', loadData: loadData   }),
-  indicators: new IndicatorStore({ key: key, sheet: 'indicators', loadData: loadData  }),
-  issues: new IssueStore({ key: key, sheet: 'issues', loadData: loadData }),
-  recommendations: new RecommendationStore({ key: key, sheet: 'recommendations', loadData: loadData })
+  routes: new RouteStore({ router: router }),
+//  data: new DataStore({ key: key, sheet: 'data', loadData: loadData  }),
+//  dataBreakdowns: new DataBreakdownStore({ key: key, sheet: 'data_breakdowns', loadData: loadData  }),
+//  dataGroups: new DataGroupStore({ key: key, sheet: 'data_groups', loadData: loadData  }),
+//  dataSources: new DataSourceStore({ key: key, sheet: 'data_sources', loadData: loadData }),
+//  dataTypes: new DataTypeStore({ key: key, sheet: 'data_types', loadData: loadData   }),
+//  indicators: new IndicatorStore({ key: key, sheet: 'indicators', loadData: loadData  }),
+//  issues: new IssueStore({ key: key, sheet: 'issues', loadData: loadData }),
+//  recommendations: new RecommendationStore({ key: key, sheet: 'recommendations', loadData: loadData })
 }
 
 //TODO styles
@@ -78,12 +74,16 @@ insertCSS(reactSelectExampleCSS)
 
 var flux = new Fluxxor.Flux(stores, actions.methods);
 
-router.run(function(Handler) {
-  React.render(
-    React.createElement(Handler, { flux: flux }),
-    document.getElementById("app")
-  );
-});
+router.run(
+  function(Handler) {
+    React.render(
+      React.createElement(Handler, { flux: flux }),
+      document.getElementById("app")
+    );
+  }
+);
+// let route store know that the router is running so that route-store can store app state
+stores.routes.initState();
 
 // boilerplate logging
 flux.on("dispatch", function(type, payload) {
